99c632920c59b2f80d20c1922a1f2ce0a5e6c518
Enak1n <masj963@gmail.com>
2023-11-09 19:20:48 +0700

    ChangeStructureOfFolders

--- E-commerce/CatalogAPI/Program.cs
+++ E-commerce/CatalogAPI/Program.cs
@@ -2,14 +2,44 @@ using Microsoft.EntityFrameworkCore;
 using CatalogAPI.Infrastructure.DataBase;
 using CatalogAPI.Infrastructure.UnitOfWork.Interfaces;
 using CatalogAPI.Infrastructure.UnitOfWork;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var databaseConnection = builder.Configuration.GetConnectionString("DbConnection");
 
+var secretKey = builder.Configuration.GetSection("JwtSettings:SecretKey").Value;
+var issuer = builder.Configuration.GetSection("JwtSettings:Issuer").Value;
+var audience = builder.Configuration.GetSection("JwtSettings:Audience").Value;
+var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
 // Add services to the container.
 builder.Services.AddDbContext<Context>(options =>
     options.UseNpgsql(databaseConnection, b => b.MigrationsAssembly("CatalogAPI")));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = issuer,
+            ValidateAudience = true,
+            ValidAudience = audience,
+            ValidateLifetime = true,
+            IssuerSigningKey = signingKey,
+            ValidateIssuerSigningKey = true,
+            ClockSkew = TimeSpan.Zero
+        };
+    }
+);
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
